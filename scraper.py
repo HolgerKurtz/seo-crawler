a4c58e5a4664f22bb324e137ced3dce6c6d5dbc3
Holger Kurtz <kulturdata@gmail.com>
2020-11-30 15:37:11 +0100

    works, but in the df there are doublicates.

--- scraper.py
+++ scraper.py
@@ -3,6 +3,7 @@ import requests
 import logging
 from pprint import pprint
 import pandas as pd
+import sys
 
 logging.basicConfig(
     filename='seo.log', 
@@ -10,48 +11,91 @@ logging.basicConfig(
     format='%(asctime)s - %(message)s',level=logging.INFO
 )
 
-class Downloader:
-    def __init__(self, url):
-        self.url = url
-        self.list_for_df = []
+def go(): # user interface for testing 
+        go = input("Go on? y/n")
+        if go == "y":
+            pass
+        elif go == "n":
+            print("Scraping wird beendet")
+            sys.exit()
+        else:
+            go()     
 
-    def load(self):
+class Downloader:
+    def __init__(self):
+        self.list_for_df = []
+        self.link_list_set = set()
+        self.scraped_list_set = set()
+        
+
+    def load(self, url):
+        self.url = url
+        logging.info(f"URL : {self.url}")
+
         page = requests.get(self.url)
-        # logging.info(f"Status : {page.status_code}")
+        logging.info(f"Status : {page.status_code}")
         self.soup = BeautifulSoup(page.content, 'html.parser', from_encoding="utf-8")
-        # print(self.soup)
+
+    def next_link(self):
+        links_list_raw = self.soup.find_all("a")
+        
+        # check if link is internal, if so : add to set
+        for link in links_list_raw:
+            try:
+                if self.url in link["href"]:
+                    # print(link["href"])
+                    self.link_list_set.add(link["href"])
+            except:
+                pass
+
 
     def find(self, *args): #! Not clean
+        logging.info(f"args/tags : {args}")
         for tag in args:
-            if tag == "description": # data is not a list so no loop or getText() needed
-                text = self.soup.find('meta', attrs={'name' : tag})["content"] 
-                text_len = len(text)
-                # Add new list with seo data to self.list_for_df
-                self.list_for_df.append([self.url, tag, text, text_len])
-            else:
-                _ = self.soup.find_all(tag)
-                for html_tag in _:
-                    text = html_tag.getText()
-                    text_len = len(text) # character count
+            try:
+                if tag == "description": # data is not a list so no loop or getText() needed
+                    text = self.soup.find('meta', attrs={'name' : tag})["content"] 
+                    text_len = len(text)
                     # Add new list with seo data to self.list_for_df
                     self.list_for_df.append([self.url, tag, text, text_len])
+                else:
+                    _ = self.soup.find_all(tag)
+                    for html_tag in _:
+                        text = html_tag.getText()
+                        text_len = len(text) # character count
+                        # Add new list with seo data to self.list_for_df
+                        self.list_for_df.append([self.url, tag, text, text_len])
+            except:
+                pass
 
 
     def create_df(self):
         columns = ["url", "html-tag" , "text", "character count"]
         df = pd.DataFrame(self.list_for_df, columns=columns)
-        return print(df)
-
-        
-
-        
+        logging.info(f"df.head() : \n{df.head()}")
+        self.create_excel(df)
+    
+    def create_excel(self, df):
+        filename = "seo.xlsx"
+        df.to_excel(filename)
+        print(f"Excel File created. Filename: {filename}")
     
         
 if __name__ == "__main__":
-    y = Downloader("https://kulturdata.de")
-    y.load()
-    y.find("title", "h1", "h2", "a", "description" )
+    domain = "https://kulturdata.de"
+    
+    y = Downloader()
+    y.load(domain)
+    y.find("title", "description")
+    y.next_link()
+
+    for url in y.link_list_set:
+        print(f"URL : {url}")
+        y.load(url)
+        y.find("title", "description")
+
     y.create_df()
+    
 
     
     
