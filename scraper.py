90897b8a668ce0f661b61a47e6b40b494143e1b8
Holger Kurtz <kulturdata@gmail.com>
2020-12-01 17:57:49 +0100

    domain now in __init__ bc cleaner code. AND : improved Log file

--- scraper.py
+++ scraper.py
@@ -22,27 +22,26 @@ def go(): # user interface for testing
         else:
             go()     
 
-class Downloader:
-    def __init__(self):
+class Scraper:
+    def __init__(self, domain):
+        self.domain = domain
         self.list_for_df = []
         self.link_list_set = set()
         self.scraped_list_set = set() # so I don't scrape urls twice 
-        
         self.graph_links_with_text = []
     
-    def load(self, url, domain):
+    def load(self, url):
         self.url = url # dynamic for recursive calling with different urls 
-        self.domain = domain
         logging.info(f"URL : {self.url}")
 
         page = requests.get(self.url)
         logging.info(f"Status : {page.status_code}")
+        
         self.soup = BeautifulSoup(page.content, 'html.parser', from_encoding="utf-8")
         self.scraped_list_set.add(self.url)
 
     def next_link(self):
         links_list_raw = self.soup.find_all("a")
-        
         # check if link is internal, if so : add to set
         for link in links_list_raw:
             try:
@@ -59,17 +58,14 @@ class Downloader:
                     pass
             except:
                 pass
-        logging.info(f"URL : {self.url}\nLinks found : \n{self.link_list_set}")
+        logging.info(f"{len(links_list_raw)} Links found : \n{links_list_raw}")
     
     def graph(self):
         df = pd.DataFrame(self.graph_links_with_text, columns=["from", "to"])
+        self.create_excel(df, "seo-graph.xlsx")
         G = nx.from_pandas_edgelist(df, source="from", target="to")
         nx.draw(G, with_labels=False)
         plt.show()
-
-        
-
-
 
     def find(self, seo_tags):
         for tag in seo_tags:
@@ -88,6 +84,7 @@ class Downloader:
                         self.list_for_df.append([self.url, tag, text, text_len])
             except:
                 pass
+
     def recurs(self):
         print("Start")
         for link in self.link_list_set.copy():
@@ -95,14 +92,15 @@ class Downloader:
                 pass
             else:
                 if requests.get(link).status_code == 200:
-                    self.load(link, domain)
+                    self.load(link)
                     self.find(seo_tags)
                     self.next_link()
                 else:
                     print(f"❌ Status Code von {link} : {requests.get(link).status_code}")
             
-        
         print(f"🗳 Anzahl an gecrawlten Links : {len(self.scraped_list_set)}")        
+        
+        # Enjoyed the Ride?
         if go():
             print("Here we go again")
             self.recurs()
@@ -113,10 +111,9 @@ class Downloader:
         columns = ["url", "html-tag" , "text", "character count"]
         df = pd.DataFrame(self.list_for_df, columns=columns)
         logging.info(f"df.head() : \n{df.head()}")
-        self.create_excel(df)
+        self.create_excel(df, "seo.xlsx")
     
-    def create_excel(self, df):
-        filename = "seo.xlsx"
+    def create_excel(self, df, filename):
         df.to_excel(filename)
         print(f"Excel File created. Filename: {filename}")
     
@@ -124,12 +121,11 @@ class Downloader:
 if __name__ == "__main__":
     url = "https://kulturdata.de"
     domain = "kulturdata.de"
-    seo_tags = ["title", "description", "h1", "h2"]
-    y = Downloader()
-    y.load(url, domain)
+    seo_tags = ["title"]
+    y = Scraper(domain)
+    y.load(url)
     y.find(seo_tags)
     y.next_link()
-
     y.recurs()
     y.graph() # not that intereseting at the moment 
     y.create_df()
